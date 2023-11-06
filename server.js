9f272e6443a444af308da4b449ee21aaab8e00ed
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-06 11:24:30 +0000

    able to create Get request Route for all resturants and it works

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const app = require("./src/app")
+const app = require("./src/app");
 const db = require("./db/connection");
+const Restaurant = require("./models");
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
-
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await Restaurant.findAll();
+  res.json(restaurants);
+});
 app.listen(port, () => {
-    db.sync();
-    console.log(`Listening at http://localhost:${port}/restaurants`);
-})
+  db.sync();
+  console.log(`Listening at http://localhost:${port}/restaurants`);
+});
